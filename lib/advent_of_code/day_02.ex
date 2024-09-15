e54da4a0cdc69e4a3a63dfaa0f4dbf1bdff60d77
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2024-09-15 16:28:06 +0200

    Day2 part2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -17,6 +17,35 @@ defmodule AdventOfCode.Day02 do
     n_2 * n_3
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> find_max("")
+  end
+
+  def find_max([_], max) do
+    max
+  end
+
+  def find_max([head | tail], max) do
+    local_max =
+      Enum.map(tail, &compare_string(head, &1))
+      |> Enum.max_by(&String.length/1)
+
+    if String.length(max) > String.length(local_max) do
+      find_max(tail, max)
+    else
+      find_max(tail, local_max)
+    end
+  end
+
+  def compare_string(str1, str2) do
+    [String.split(str1, "", trim: true), String.split(str2, "", trim: true)]
+    |> Enum.zip()
+    |> Enum.flat_map(fn
+      {c, c} -> [c]
+      {_, _} -> []
+    end)
+    |> Enum.join("")
   end
 end
