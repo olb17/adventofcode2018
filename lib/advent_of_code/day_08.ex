c282e2005ad54ca8e2a176ba24017c2a57de5808
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2024-10-01 21:52:59 +0200

    day08p2

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,12 +1,15 @@
 defmodule AdventOfCode.Day08 do
-  def part1(args) do
+  def part1(args), do: find_sol(args, &compute_metas/1)
+
+  def part2(args), do: find_sol(args, &compute_node/1)
+
+  defp find_sol(args, compute) do
     args
     |> String.trim()
     |> String.split(" ", trim: true)
     |> Enum.map(&String.to_integer/1)
     |> parse_node()
-    |> dbg
-    |> then(fn {_, node} -> compute_metas(node) end)
+    |> then(fn {_, node} -> compute.(node) end)
   end
 
   defp parse_node(incoming) do
@@ -23,20 +26,24 @@ defmodule AdventOfCode.Day08 do
       end
 
     {metas, rest} = Enum.split(rest, nb_meta)
-    {rest, %{nodes: nodes, metas: metas}}
+    {rest, %{nodes: nodes |> Enum.reverse(), metas: metas}}
   end
 
   defp compute_metas(node) do
-    a =
-      Enum.reduce(node.nodes, 0, fn node, acc ->
-        acc + compute_metas(node)
-      end)
-
-    b = Enum.sum(node.metas)
-
-    a + b
+    Enum.reduce(node.nodes, Enum.sum(node.metas), fn node, acc ->
+      acc + compute_metas(node)
+    end)
   end
 
-  def part2(_args) do
+  defp compute_node(nil), do: 0
+
+  defp compute_node(node) when length(node.nodes) == 0 do
+    Enum.sum(node.metas)
+  end
+
+  defp compute_node(node) do
+    Enum.reduce(node.metas, 0, fn meta, acc ->
+      acc + compute_node(Enum.at(node.nodes, meta - 1))
+    end)
   end
 end
